6f1403731bcced100827e15654744659fc139750
Bee Martinez <beemrtinez@gmail.com>
2017-02-24 18:45:47 -0600

    i suck at the command line
    Using desktop Github to create a repo. Maybe. I’m not sure.

---
+++ src/components/Dashboard.vue
@@ -0,0 +1,22 @@
+<template>
+<div>
+<h1>{{ header }}</h1>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'dashboard',
+  data () {
+    return {
+      header: 'Welcome to Movie Phone'
+    }
+  }
+}
+</script>
+
+<style>
+h1 {
+  color: blue;
+}
+</style>
