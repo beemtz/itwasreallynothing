caff3c58bd49c1cdd54b49216188ef19cc45de18
Bee Martinez <beemrtinez@gmail.com>
2017-02-27 15:11:11 -0600

    SiteFooter and AsideBar components added

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -2,65 +2,23 @@
 <div class="container">
 <navigation>
 </navigation>
-    <aside class="menu">
-  <p class="menu-label">
-    General
-  </p>
-  <ul class="menu-list">
-    <li><a>Dashboard</a></li>
-    <li><a>Customers</a></li>
-  </ul>
-  <p class="menu-label">
-    Administration
-  </p>
-  <ul class="menu-list">
-    <li><a>Team Settings</a></li>
-    <li>
-      <a class="is-active">Manage Your Team</a>
-      <ul>
-        <li><a>Members</a></li>
-        <li><a>Plugins</a></li>
-        <li><a>Add a member</a></li>
-      </ul>
-    </li>
-    <li><a>Invitations</a></li>
-    <li><a>Cloud Storage Environment Settings</a></li>
-    <li><a>Authentication</a></li>
-  </ul>
-  <p class="menu-label">
-    Transactions
-  </p>
-  <ul class="menu-list">
-    <li><a>Payments</a></li>
-    <li><a>Transfers</a></li>
-    <li><a>Balance</a></li>
-  </ul>
-</aside>
-<footer class="footer">
-  <div class="container">
-    <div class="content has-text-centered">
-      <p>
-        <strong>Bulma</strong> by <a href="http://jgthms.com">Jeremy Thomas</a>. The source code is licensed
-        <a href="http://opensource.org/licenses/mit-license.php">MIT</a>. The website content
-        is licensed <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/">CC ANS 4.0</a>.
-      </p>
-      <p>
-        <a class="icon" href="https://github.com/jgthms/bulma">
-          <i class="fa fa-github"></i>
-        </a>
-      </p>
-    </div>
-  </div>
-</footer>
+<aside-bar>
+</aside-bar>
+<site-footer>
+</site-footer>
 </div>
 </template>
 
 <script>
 import Navigation from './Navigation'
+import SiteFooter from './SiteFooter'
+import AsideBar from './AsideBar'
 export default {
   name: 'dashboard',
   components: {
-    Navigation
+    Navigation,
+    SiteFooter,
+    AsideBar
   }
 }
 </script>
---
+++ src/components/SiteFooter.vue
@@ -0,0 +1,28 @@
+<template>
+  <footer class="footer">
+    <div class="container">
+      <div class="content has-text-centered">
+        <p>
+          <strong>Anything</strong> by <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" target="_blank">Some Cool People</a>
+        </p>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'site-footer',
+  data () {
+    return {
+      header: 'Welcome to Movie Phone'
+    }
+  }
+}
+</script>
+
+<style>
+h1 {
+  color: blue;
+}
+</style>
