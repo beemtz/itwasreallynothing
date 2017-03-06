80428b32d49157c01787b731de64c76b79e5b729
Bee Martinez <beemrtinez@gmail.com>
2017-03-06 12:18:01 -0600

    changed aside bar, new dashboard, site footer new margings, other stuff

--- src/components/AsideBar.vue
+++ src/components/AsideBar.vue
@@ -1,38 +1,16 @@
 <template>
-  <aside class="menu">
-<p class="menu-label">
-  General
-</p>
-<ul class="menu-list">
-  <li><a>Dashboard</a></li>
-  <li><a>Customers</a></li>
-</ul>
-<p class="menu-label">
-  Administration
-</p>
-<ul class="menu-list">
-  <li><a>Team Settings</a></li>
-  <li>
-    <a class="is-active">Manage Your Team</a>
-    <ul>
-      <li><a>Members</a></li>
-      <li><a>Plugins</a></li>
-      <li><a>Add a member</a></li>
-    </ul>
-  </li>
-  <li><a>Invitations</a></li>
-  <li><a>Cloud Storage Environment Settings</a></li>
-  <li><a>Authentication</a></li>
-</ul>
-<p class="menu-label">
-  Transactions
-</p>
-<ul class="menu-list">
-  <li><a>Payments</a></li>
-  <li><a>Transfers</a></li>
-  <li><a>Balance</a></li>
-</ul>
+  <class="column is-3">
+    <aside class="menu">
+      <p class="menu-label">Aside Title</p>
+        <ul class="menu-list">
+          <li><a>Item</a></li>
+          <li><a>Other Item</a></li>
+          <li><a>Thing</a></li>
+          <li><a>Other Thing</a></li>
+          <li><a>Thingy Item</a></li>
+        </ul>
 </aside>
+</class>
 </template>
 
 <script>
