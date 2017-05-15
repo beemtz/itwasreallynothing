c4832a1a90171fb538510f982eddb8b56654edcd
Bee Martinez <beemrtinez@gmail.com>
2017-05-14 19:53:34 -0500

    changed menus to be js based

--- src/components/AsideBar.vue
+++ src/components/AsideBar.vue
@@ -3,11 +3,13 @@
     <aside class="menu">
       <p class="menu-label">Aside Title</p>
       <ul class="menu-list">
-        <li><a>Item</a></li>
-        <li><a>Other Item</a></li>
-        <li><a>Thing</a></li>
-        <li><a>Other Thing</a></li>
-        <li><a>Thingy Item</a></li>
+        <li><a class="special-item">  <a
+          :href="stuff.slug"
+          v-for="stuff in asideStuff"
+          class="nav-item">
+          {{ stuff.name }}
+        </a></li>
+        <!-- ISSUE HERE! Since it's all in a single <li>/<a>, everything moves at the same time-->
       </ul>
     </aside>
   </div>
@@ -18,18 +20,51 @@ export default {
   name: 'aside-bar',
   data () {
     return {
-      header: 'Welcome to Movie Phone'
+      header: 'Welcome to Movie Phone',
+      asideStuff: [
+        {
+          name: 'Item'
+        },
+        {
+          name: 'Other Item'
+        },
+        {
+          name: 'Thing'
+        },
+        {
+          name: 'Other Thing'
+        },
+        {
+          name: 'Thingy Item'
+        }
+      ]
     }
   }
 }
 </script>
 
 <style lang="scss" scoped>
+@import "~assets/styles/vars";
 
-$letterscolor: white; /*why is this getting overwritten?*/
+$letterscolor: white;
 
-h1 {
-  color: $letterscolor;
+ul.menu-list {
+background-color: $info;
+  a {
+    position: relative;
+    left: 0;
+    transition: all 2000ms;
+    background-color: transparent;
+    color: $letterscolor;
+    &.special-item {
+      color: $primary;
+    }
+    &:hover {
+      left: 100px;
+      background-color: $danger;
+      color: $warning;
+    }
+  }
 }
 
 </style>
--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,28 +1,26 @@
 <template>
-<nav class="nav">
-  <div class="nav-left">
-    <a class="nav-item">
-      <h1>{{ header }}</h1>
-    </a>
-  </div>
-  <!-- This "nav-toggle" hamburger menu is only visible on mobile -->
-  <!-- You need JavaScript to toggle the "is-active" class on "nav-menu" -->
-  <span class="nav-toggle">
-    <span></span>
-    <span></span>
-    <span></span>
-  </span>
-  <!-- This "nav-menu" is hidden on mobile -->
-  <!-- Add the modifier "is-active" to display it on mobile -->
-  <div class="nav-right nav-menu">
-    <a class="nav-item">
-      Home
-    </a>
-    <a class="nav-item">
-      Documentation
-    </a>
-    <a class="nav-item">
-      Blog
+  <nav class="nav">
+    <div class="nav-left">
+      <a class="nav-item">
+        <h1>{{ header }}</h1>
+      </a>
+    </div>
+    <!-- This "nav-toggle" hamburger menu is only visible on mobile -->
+    <!-- You need JavaScript to toggle the "is-active" class on "nav-menu" -->
+    <span class="nav-toggle">
+      <span></span>
+      <span></span>
+      <span></span>
+    </span>
+    <!-- This "nav-menu" is hidden on mobile -->
+    <!-- Add the modifier "is-active" to display it on mobile -->
+    <div class="nav-right nav-menu">
+      <a
+      :href="thing.slug"
+      v-for="thing in menuThings"
+      v-if="thing.slug"
+      class="nav-item">
+      {{ thing.name }}
     </a>
   </div>
 </nav>
@@ -33,7 +31,21 @@ export default {
   name: 'navigation',
   data () {
     return {
-      header: 'Welcome to Movie Phone'
+      header: 'Welcome to Movie Phone',
+      menuThings: [
+        {
+          name: 'Home',
+          slug: 'home'
+        },
+        {
+          name: 'Documentation',
+          slug: 'docs'
+        },
+        {
+          name: 'Blog',
+          slug: 'blog'
+        }
+      ]
     }
   }
 }
